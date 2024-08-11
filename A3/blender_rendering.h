711027ce1cae8418650f13cd2c342788322cbd37
yvychivy <98163289+yvich@users.noreply.github.com>
2024-08-11 14:18:42 -0400

    Update rendering.h

--- A3/blender_rendering.h
+++ A3/blender_rendering.h
@@ -1,23 +1,23 @@
-#ifndef BLENDER_RENDERING_H
-#define BLENDER_RENDERING_H
+#ifndef RENDERING_H
+#define RENDERING_H
 
 #include <GL/glew.h>
+#include <glm/glm.hpp>
 #include <string>
 #include <vector>
-#include <glm/glm.hpp> 
 
 // Structure to represent a vertex
 struct Vertex {
-    glm::vec3 position;  // Position of the vertex
-    glm::vec3 normal;    // Normal vector at the vertex
-    glm::vec2 texCoords; // Texture coordinates
+    glm::vec3 position;
+    glm::vec3 normal;
+    glm::vec2 texCoords;
 };
 
 // Structure to represent a mesh
 struct Mesh {
-    std::vector<Vertex> vertices;  // Array of vertices
-    std::vector<GLuint> indices;   // Array of indices for indexed drawing
-    GLuint VAO;                    // Vertex Array Object identifier
+    std::vector<Vertex> vertices;
+    std::vector<GLuint> indices;
+    GLuint VAO;
 };
 
 // Function to load an OBJ file and return a Mesh object
@@ -40,4 +40,4 @@ GLuint createMeshVAO(const Mesh& mesh);
 //   shaderProgram - The OpenGL shader program to use for rendering
 void renderMesh(const Mesh& mesh, GLuint shaderProgram);
 
-#endif // BLENDER_RENDERING_H
+#endif // RENDERING_H
